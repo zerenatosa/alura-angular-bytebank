4d5eda53e81caa76adf4ceae4ac14ae6f0942466
Jose Renato Santiago Arantes <zerenatosa@gmail.com>
2022-08-10 23:16:04 -0300

    10 08 2022 - fim de noite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  destino: number;
+  valor: number;
+
+
+
+  transferir($event){
+    alert('cheguei aqui no event' + $event.destino);
+    console.log($event)
+    this.destino = $event.destino;
+    this.valor = $event.valor;
+    alert(this.destino + "olha");
+  }
+
+
+
+
 }
